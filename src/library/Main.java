25be5ddadf4787dde5f0d1f1407f1eae48c98e30
sayyed qaiser <sayyedqaiser03@gmail.com>
2018-10-09 14:51:13 +0530

    tabpane added

--- src/library/Main.java
+++ src/library/Main.java
@@ -6,17 +6,9 @@
 package library;
 import javafx.application.Application; 
 import javafx.scene.Scene; 
-import javafx.scene.control.Button; 
-import javafx.scene.layout.*; 
-import javafx.event.ActionEvent; 
-import javafx.event.EventHandler; 
 import javafx.scene.control.*; 
 import javafx.stage.Stage; 
-import javafx.scene.control.Alert.AlertType; 
-import java.time.LocalDate; 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import library.Student;
+import library.Student.*;
 /**
  *
  * @author qaiser
@@ -26,63 +18,26 @@ public class Main extends  Application {
     @Override
     public void start(Stage stage) throws Exception {
         
-        // create a menu 
-        Menu menu = new Menu("Students"); 
-        Menu menu2 = new Menu("Books");
-        Menu menu3 = new Menu("Borrow");
-        Menu menu4 = new Menu("Return");
-  
-        // create menuitems 
-        MenuItem m1 = new MenuItem("Add Student"); 
-        MenuItem m2 = new MenuItem("View Students");
-        MenuItem m3 = new MenuItem("Add Books");
-        MenuItem m4 = new MenuItem("View Books");
-        MenuItem m5 = new MenuItem("Borrow Book");
-        MenuItem m6 = new MenuItem("View Borrowed Books");
-        MenuItem m7 = new MenuItem("View Returned Books");
-        MenuItem m8 = new MenuItem("View Book");
+         TabPane tp = new TabPane();
+        Tab tab1 = new Tab();
+        Tab tab2 = new Tab();
+        Tab tab3 = new Tab();
+        Tab tab4 = new Tab();
+        
+        tab1.setText("Student");
+        tab2.setText("Books");
+        tab3.setText("Borrow");
+        tab4.setText("Return");
         
         
         
-  
-        // add menu items to menu 
-        menu.getItems().add(m1); 
-        menu.getItems().add(m2); 
-        
-        menu2.getItems().add(m3);
-        menu2.getItems().add(m4);
-        
-        menu3.getItems().add(m5);
-        menu3.getItems().add(m6);
-        
-        menu4.getItems().add(m7);
-        menu4.getItems().add(m8);
-  
-  
-        // create events for menu items 
-       m1.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                    System.out.println("clicked");
-                   
-            }
-       });
-    
-    
+        tp.getTabs().add(tab1);
+        tp.getTabs().add(tab2);
+        tp.getTabs().add(tab3);
+        tp.getTabs().add(tab4);
        
-  
-        // create a menubar 
-        MenuBar mb = new MenuBar(); 
-  
-        // add menu to menubar 
-        mb.getMenus().add(menu);
-        mb.getMenus().add(menu2);
-        mb.getMenus().add(menu3);
-        mb.getMenus().add(menu4);
-
         // create a scene 
-        Scene sc = new Scene(mb, 500, 300); 
+        Scene sc = new Scene(tp, 500, 300); 
   
         // set the scene 
         stage.setScene(sc); 
