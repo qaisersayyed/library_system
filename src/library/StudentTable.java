ae671f4a94513ea6495c369d1fcbfef196ce15d8
sayyed qaiser <sayyedqaiser03@gmail.com>
2018-10-12 12:09:37 +0530

    student table editing

--- src/library/StudentTable.java
+++ src/library/StudentTable.java
@@ -5,7 +5,13 @@
  */
 package library;
 
+import Database.Database;
+import Database.StudentController;
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.stage.Stage;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -14,52 +20,26 @@ import javax.swing.JTable;
  *
  * @author qaiser
  */
-public class StudentTable {
-   
-    JFrame f;    
-    StudentTable(){    
-    // frame 
-    JFrame f; 
-    // Table 
-    JTable j; 
-  
-    // Constructor 
-   
-  
-        // Frame initiallization 
-        f = new JFrame(); 
-  
-        // Frame Title 
-        f.setTitle("JTable Example"); 
+public class StudentTable extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Database d = new  Database();
+        Connection con = d.openConnection();
+        StudentController.getstudent(con);
+        ArrayList<StudentController> s=StudentController.getstudent(con);
+        for (int i=0;i<s.size();i++){
+            System.out.println(s.get(i).getName());
+            System.out.println(s.get(i).getPhone());
+            System.out.println(s.get(i).getEmail());
+        }
         
         
-        // Data to be displayed in the JTable 
-        String[][] data = { 
-            { "Kundan Kumar Jha", "4031", "CSE" }, 
-            { "Anand Jha", "6014", "IT" } 
-        }; 
-  
-        // Column Names 
-        String[] columnNames = { "Name", "Roll Number", "Department" }; 
-  
-        // Initializing the JTable 
-        j = new JTable(data, columnNames); 
-        j.setBounds(30, 40, 200, 300); 
-  
-        // adding it to JScrollPane 
-        JScrollPane sp = new JScrollPane(j); 
-        f.add(sp); 
-        // Frame Size 
-        f.setSize(500, 200); 
-        // Frame Visible = true 
-        f.setVisible(true); 
+    }
+   
+public static void main(String[] args) {
+        launch(args);
+    }
    
 }   
-public static void main(String[] args) {    
-    new StudentTable();    
-}    
 
-    public StudentTable(String name, String email, String phone) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-}
